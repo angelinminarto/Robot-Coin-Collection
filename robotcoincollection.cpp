553612eda632ab68f4cc9923f3d9f93c3766c720
veronika angel <74860907+angelinminarto@users.noreply.github.com>
2021-10-16 19:00:11 +0700

    Add files via upload

---
+++ robotcoincollection.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+// //looping menambah angka yg sebelumnya
+int main ()
+ {
+   int n;
+   int m;
+   char [100][100];
+ scanf("%d",&n);
+ scanf("%d",&m);
+ for(int i = n; i <= m; i+=n){
+   for(i=0; i<n;i<m;i++){
+     scanf("%d",&n); getchar();
+   }
+ }
+
+ printf("%d",i);
+   return 0;
+ }
